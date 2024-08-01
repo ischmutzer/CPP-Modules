7bb93e1dbfa1fbf7c0600c59c795875963d91922
Ildiko Schmutzer <136725723+ischmutzer@users.noreply.github.com>
2024-08-01 17:44:03 +0200

    Add files via upload

---
+++ CPP Module 00/ex00/Megaphone.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ischmutz <ischmutz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 14:11:01 by ischmutz          #+#    #+#             */
+/*   Updated: 2024/08/01 14:33:28 by ischmutz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		int	i = 0;
+		while (argv[++i])
+		{
+			int j = -1;
+			while (argv[i][++j])
+				argv[i][j] = std::toupper(argv[i][j]);
+			std::cout << argv[i];
+			if ((i + 1) == argc)
+				continue ;
+			std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
