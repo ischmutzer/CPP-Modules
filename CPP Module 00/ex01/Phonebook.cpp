ae2f1e4eec1c572bd6bae7c954fdc46975baeb9e
willem <willemvdvorst@gmail.com>
2024-08-18 21:47:07 +0200

    while(true) loop added and std::cin is being used to read input from the user

--- CPP Module 00/ex01/Phonebook.cpp
+++ CPP Module 00/ex01/Phonebook.cpp
@@ -3,6 +3,23 @@
 
 int main(int argc, char **argv)
 {
+    std::string input;
+
     if (argc != 1) 
         std::cout << "Wrong number of arguments" << std::endl;
+    std::cout << "Welcome to the phonebook" << std::endl;
+    while (true)
+    {
+        std::cout << "What do you want to do" << std::endl;
+        std::cin >> input;
+        if (input == "ADD") {
+            //add contact
+        }
+        else if (input == "SEARCH") {
+            //search
+        }
+        else if (input == "EXIT") {
+            break ;
+        }
+    }
 }
