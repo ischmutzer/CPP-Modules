935a671a409be4685cbd8fcdd049a8cc44c17915
willem <willemvdvorst@gmail.com>
2024-08-20 17:34:38 +0200

    added destructor and add(Phonebook *phonebook) ft in main.cpp // adding contact should now work and index 0 should be overwritten succesfully when index < 8

---
+++ CPP00/ex01/Contacts.cpp
@@ -0,0 +1,56 @@
+#include "Phonebook.hpp"
+//#include <iostream>
+
+Contacts::Contacts() {
+    _firstName = "";
+     _lastName = "";
+     _nickname = "";
+     _phoneNumber = "";
+     _secret = "";
+}
+
+Contacts::Contacts(std:: string firstName, std:: string lastName, std:: string nickname, std:: string phoneNumber, std:: string secret) {
+    _firstName = firstName;
+    _lastName = lastName;
+    _nickname = nickname;
+    _phoneNumber = phoneNumber;
+    _secret = secret;
+}
+
+std::string Contacts::getFirstName() {
+    return _firstName;
+}
+std::string Contacts::getLastName() {
+    return _lastName;
+}
+std::string Contacts::getNickName() {
+    return _nickname;
+}
+
+std::string Contacts::getPhoneNumber() {
+    return _phoneNumber;
+}
+
+std::string Contacts::getSecret() {
+    return  _secret;
+}
+
+void    Contacts::setFirstName(std::string _firstName) {
+    this->_firstName = _firstName;
+}
+
+void    Contacts::setLastName(std::string str) {
+    _lastName = str;
+}
+
+void    Contacts::setNickname(std::string str) {
+    _nickname = str;
+}
+
+void    Contacts::setPhoneNumber(std::string str) {
+    _phoneNumber = str;
+}
+
+void    Contacts::setSecret(std::string str) {
+    _secret = str;
+}
---
+++ CPP00/ex01/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include "Phonebook.hpp"
+
+int    add(Phonebook *phonebook) {
+    std::string input = "";
+    std::string firstName = "";
+    std::string lastName = "";
+    std::string nickname = "";
+    std::string phoneNumber = "";
+    std::string secret = "";
+
+    while (firstName.empty() || lastName.empty() || nickname.empty() || phoneNumber.empty() || secret.empty()) {
+        if (!std::getline(std::cin, input))
+            return 1;
+        else if (firstName.empty())
+            firstName = input;
+        else if (lastName.empty())
+            lastName = input;
+        else if (nickname.empty())
+            nickname = input;
+        else if (phoneNumber.empty()) {
+            // should I check the phone number for some sort of validity?
+            phoneNumber = input;
+        }
+        else if (secret.empty())
+            secret = input;
+        else {
+            std::cout << "Please give an input" << std::endl; //broken
+            continue ;
+        }
+        phonebook->addContact(firstName, lastName, nickname, phoneNumber, secret);
+    }
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    (void)argv;
+    std::string input;
+    Phonebook   phonebook;
+
+    if (argc != 1) 
+        std::cout << "Wrong number of arguments" << std::endl;
+    std::cout << "Welcome to the phonebook" << std::endl;
+    while (true)
+    {
+        std::cout << "Please input ADD, SEARCH or EXIT" << std::endl;
+        if (!std::getline(std::cin, input)) {
+            return 1 ;
+        }
+        if (input == "ADD") {
+            if (add(&phonebook))
+                return 1;
+        }
+        else if (input == "SEARCH") {
+            //search
+        }
+        else if (input == "EXIT") {
+            break ;
+        }
+        else {
+            continue;
+        }
+    }
+}
