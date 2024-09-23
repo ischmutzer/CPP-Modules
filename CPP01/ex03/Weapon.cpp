def2e9745b6c62774ac4a47cb21595e76b7bb079
ildiko <jasminaylik@gmail.com>
2024-09-24 01:41:25 +0200

    added private attribute type to Weapon class and get and set functions

--- CPP01/ex03/Weapon.cpp
+++ CPP01/ex03/Weapon.cpp
@@ -5,5 +5,13 @@ Weapon::Weapon() {
 }
 
 Weapon::~Weapon() {
-    
+
+}
+
+const std::string&  Weapon::getType() {
+
+}
+
+void    Weapon::setType(std::string newType) {
+	_type = newType;
 }
--- CPP01/ex03/Weapon.hpp
+++ CPP01/ex03/Weapon.hpp
@@ -1,12 +1,17 @@
 #ifndef WEAPON_HPP
 #define WEAPON_HPP
 
+#include <string>
+
 class	Weapon {
 	private:
+		std::string	_type;
 
 	public:
 		Weapon();
 		~Weapon();
+		const std::string&	getType();
+		void				setType(std::string newType);
 };
 
 #endif
