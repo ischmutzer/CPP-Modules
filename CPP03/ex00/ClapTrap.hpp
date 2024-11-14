b78cb3c18b674e485b946802a8d23cd7b7d01015
Ildiko Jasmin Schmutzer <ischmutz@c2r5p5.42vienna.com>
2024-11-14 15:57:20 +0100

    ex00 done. Delimitors: health cannot be under 0 or over 10

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -10,15 +10,23 @@ class ClapTrap {
 		ClapTrap&	operator=(const ClapTrap& source);
 		~ClapTrap();
 
+		std::string	getName();
+		int			getHealth();
+		int			getEnergy();
+		int			getAttackDamage();
+
+		void	setHealth(unsigned int amount, int mode);
+		void	setEnergy(unsigned int amount);
+
 		void	attack(const std::string& target);
 		void	takeDamage(unsigned int amount);
 		void	beRepaired(unsigned int amount);
 
 	private:
 		std::string	_name;
-		int			_hitPoints;
-		int			_energyPoints;
-		int			_attackDamage;
+		unsigned int			_hitPoints;
+		unsigned int			_energyPoints;
+		unsigned int			_attackDamage;
 };
 
 #endif
