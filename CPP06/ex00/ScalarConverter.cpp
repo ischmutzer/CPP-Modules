dc0e91f64f66637cd16c0d12d93971cf4ed9c0e1
Ildiko Jasmin Schmutzer <ischmutz@c2r5p5.42vienna.com>
2024-12-04 18:28:57 +0100

    am I supposed to only have the convert() ft or can I break down the parsing in other fts?

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -1,1 +1,52 @@
 #include "ScalarConverter.hpp"
+#include <sstream>
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(ScalarConverter& source) {
+	*this = source;
+}
+
+ScalarConverter&	ScalarConverter::operator=(ScalarConverter& source) {
+	if (this != &source) {
+		//copy
+	}
+	return *this;
+}
+
+ScalarConverter::~ScalarConverter() {}
+
+bool	ScalarConverter::intCheck(std::string& input) {
+	std::stringstream	ss(input);
+	
+	int	value;
+	if (!(ss >> value))
+		return false;
+
+	char	leftover;
+	if (ss >> leftover)
+		return false;
+	ss >> intVal;
+	return true;
+}
+
+bool	ScalarConverter::floatCheck(std::string input) {
+	if (input == ".f")
+		return true; //convert
+	int	i = 0;
+	while (i < input.length())
+}
+
+void	ScalarConverter::checkInput(std::string& input) {
+	if (intCheck(input))
+		//CONVERT VAL & store
+}
+
+void	ScalarConverter::convert(std::string input) {
+	//do
+	checkInput(input);
+}
+
+// You have to first detect the type of the literal passed as parameter, convert it from
+// string to its actual type, then convert it explicitly to the three other data types. Lastly,
+// display the results as shown below
--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -4,7 +4,27 @@
 
 class	ScalarConverter {
 	public:
-		virtual void convert(std::string string) const = 0;
+		virtual ~ScalarConverter();
+
+		static void convert(std::string input);
+		static void	checkInput(std::string& input);
+		// static bool	charCheck(std::string input);
+		static bool	intCheck(std::string& input);
+		static bool	floatCheck(std::string input);
+		// static bool	doubleCheck(std::string input);
+
 	protected:
 	private:
+		ScalarConverter();
+		ScalarConverter(ScalarConverter& source);
+		ScalarConverter&	operator=(ScalarConverter& source);
+
+		static char		charVal;
+		static int		intVal;
+		static float	floatVal;
+		static double	doubleVal;
 };
+
+
+//making constructor priavte acomplishes the same as virtual (type) ft() const = 0
+//-> it makes the class abstract 
