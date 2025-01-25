6009e58d444f387bca8ba640225324ca1ef00b43
UnhealthyHuman <jasminaylik@gmail.com>
2025-01-25 14:42:15 +0100

    at runtime compiler will throw an error if data type is mismatched since I only have one typename T

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -1,6 +1,27 @@
 #include "whatever.hpp"
 
 int	main() {
-	//a
+	int a = 2, b = 8;
+	double c = 2.2, d = 8.8;
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "Int swap: ";
+	swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	std::cout << "\nc = " << c << ", d = " << d << std::endl;
+	std::cout << "Double swap: ";
+	swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+
+	std::cout << "\nMax of a and b: " << max(a, b) << std::endl;
+	std::cout << "Min of a and b: " << min(a, b) << std::endl;
+
+	std::cout << "\nMax of c and d: " << max(c, d) << std::endl;
+	std::cout << "Min of c and d: " << min(c, d) << std::endl;
+	
+	// std::cout << "Mismatched data types: " << std::endl;
+	// swap(a, c);
+
 	return 0;
 }
--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -1,7 +1,19 @@
 #pragma once
 
+#include <iostream>
+
+/* template <typename T>
+bool	checkDataType(T, T) {
+	return true;
+}
+
+template <typename T1, typename T2>
+bool	checkDataType(T1, T2) {
+	return false;
+} */
+
 template <typename T>
-T swap(T a, T b) {
+void swap(T& a, T& b) {
 	T tmp = a;
 	a = b;
 	b = tmp;
@@ -9,18 +21,12 @@ T swap(T a, T b) {
 
 template <typename T>
 T	min(T a, T b) {
-	if (a == b || a > b)
-		return b;
-	if (a < b)
-		return a;
+	return (a < b) ? a : b;
 }
 
 template <typename T>
 T	max(T a, T b) {
-	if (a == b || b > a)
-		return b;
-	else
-		return a;
+	return (a > b) ? a : b; //if a is bigger than b return a, otherwise b
 }
 
 //NOTES
