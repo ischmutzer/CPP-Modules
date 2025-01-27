df36f843f8d824ec7cc496eb8e669b776dd93b66
Ildiko Jasmin Schmutzer <ischmutz@c2r7p5.42vienna.com>
2025-01-27 13:43:36 +0100

    ex01 done -> needs further tests added to main, add a arrLength fta s well

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,6 +1,31 @@
 #pragma once
 
+#include <cstddef>
 #include <iostream>
 
-template <typename T1, typename T2, typename T3>
-void iter(T1, T2, T3) {}
+template <typename T, typename func>
+void iter(T* arrayAddress, size_t arrayLength, func function) {
+	for (size_t i = 0; i < arrayLength; i++) {
+		function(arrayAddress[i]);
+	}
+}
+
+//test functions
+
+#include <typeinfo>
+
+template <typename T>
+void	print(T* arr, size_t size) {
+	for (size_t i = 0; i < size; i++) {
+		std::cout << typeid(arr[i]).name() << " " << arr[i] << std::endl;
+	}
+}
+
+
+//NOTES
+
+//instead of <typename T1, typename T2, typename T3>
+//<typename T, typename func> T is so that arrayAddress can be a ptr
+//to any type of memory address, int*, double*, class*, etc
+//The template parameter Func allows the compiler to deduce the
+//correct type for whatever kind of callable object is passed
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -1,5 +1,14 @@
 #include "iter.hpp"
+#include <iostream>
+
+void	printArray(std::string& str) {
+	std::cout << str << std::endl;
+}
 
 int main() {
+    std::string strs[5] = {"hola", "hello", "hallo", "a"};
+
+    iter(strs, 4, printArray);
+    //iter(strs, 5, print);
     return 0;
 }
