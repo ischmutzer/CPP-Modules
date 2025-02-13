da8afd23622e29bd1762dac7325fac0769fe349f
Ildiko Jasmin Schmutzer <ischmutz@c4r5p10.42vienna.com>
2025-02-13 12:09:37 +0100

    added tests to ex01

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -1,36 +1,6 @@
 #include <iostream>
 #include "Serializer.hpp"
 
-/* int main() {
-    Data* original = new Data;
-    original->num = 42;
-    original->str = "aaaaaa!";
-    
-    // Print original values
-    std::cout << "Original pointer: " << original << std::endl;
-    std::cout << "Original values: " << original->num << ", " << original->str << std::endl;
-    
-    // Serialize
-    uintptr_t serialized = Serializer::serialize(original);
-    std::cout << "Serialized value: " << serialized << std::endl;
-    
-    // Deserialize
-    Data* deserialized = Serializer::deserialize(serialized);
-    std::cout << "Deserialized pointer: " << deserialized << std::endl;
-    std::cout << "Deserialized values: " << deserialized->num << ", " << deserialized->str << std::endl;
-    
-    // Compare pointers
-    if (original == deserialized)
-        std::cout << "Success: Pointers match!" << std::endl;
-    else
-        std::cout << "Error: Pointers don't match!" << std::endl;
-    
-    // Clean up
-    delete original;
-    
-    return 0;
-}; */
-
 int main() {
     // Test Case 1: Basic test with a Data object
     Data* original1 = new Data;
@@ -195,3 +165,33 @@ int main() {
 
     return 0;
 }
+
+/* int main() {
+    Data* original = new Data;
+    original->num = 42;
+    original->str = "aaaaaa!";
+    
+    // Print original values
+    std::cout << "Original pointer: " << original << std::endl;
+    std::cout << "Original values: " << original->num << ", " << original->str << std::endl;
+    
+    // Serialize
+    uintptr_t serialized = Serializer::serialize(original);
+    std::cout << "Serialized value: " << serialized << std::endl;
+    
+    // Deserialize
+    Data* deserialized = Serializer::deserialize(serialized);
+    std::cout << "Deserialized pointer: " << deserialized << std::endl;
+    std::cout << "Deserialized values: " << deserialized->num << ", " << deserialized->str << std::endl;
+    
+    // Compare pointers
+    if (original == deserialized)
+        std::cout << "Success: Pointers match!" << std::endl;
+    else
+        std::cout << "Error: Pointers don't match!" << std::endl;
+    
+    // Clean up
+    delete original;
+    
+    return 0;
+}; */
