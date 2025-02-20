906ed59b8ec9d050b36392c4b937f4e9d6fb2031
Ildiko Jasmin Schmutzer <ischmutz@c4r5p4.42vienna.com>
2025-02-20 16:14:53 +0100

    added simply try catch block for main

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -1,3 +1,14 @@
 #pragma once
 
+#include <exception>
+#include <iostream>
+#include <map>
+
+class	btc {
+	public:
+	protected:
+	private:
+	std::map<int, int>	map;
+};
+
 //std::map
--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -1,3 +1,14 @@
 #include "BitcoinExchange.hpp"
+#include <cstdlib>
 
-int main() {}
+int main(int argc, char** argv) {
+	if (argc == 2) {
+		try {
+			//Implementation
+			EXIT_SUCCESS;
+		} catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	EXIT_FAILURE;
+}
