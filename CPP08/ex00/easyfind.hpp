9e649b693e47d383fbd0fb99f04406b8a2ef50bc
Ildiko Jasmin Schmutzer <ischmutz@c1r2p10.42vienna.com>
2025-04-24 14:38:19 +0200

    added comments to hpp file and checked against eval sheet

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -7,7 +7,7 @@
 #include <sstream>
 
 template <typename T>
-typename T::const_iterator	easyfind(const T& container, int x) {
+typename T::const_iterator	easyFind(const T& container, int x) {
 	typename T::const_iterator index;
 	index = std::find(container.begin(), container.end(), x);
 	if (index != container.end()) {
@@ -30,3 +30,13 @@ typename T::const_iterator	easyfind(const T& container, int x) {
 //template ft syntax: template < parameter-list > function-declaration
 
 //typename keyword -> defines template parameters (can be replaced bz keyword <class>)
+
+
+//T::const_iterator is an iterator with read-only permissions, user can only
+//read but not modify elements of the container (unless they somehow typecast the const away)
+//T::const_iterator is a dependent name therefore you need the typename keyword until T gets replaced by an actual data type
+
+//std::find() searches for x from the given iterators. If it finds x, it'll return an it
+//to the element, otherise it'll return an it pointing to container.end()
+
+//std::ostringstream creates a string stream (from <sstream>) and we can write into it with the << operator, like std::cout
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -3,6 +3,8 @@
 #include <list>
 
 int main() {
+
+	//TESTING WITH VECTOR CONTAINER
 	std::vector<int>	vec1(5);
 	vec1.push_back(17);
 	vec1.push_back(5);
@@ -11,14 +13,16 @@ int main() {
 	vec1.push_back(-12);
 
 	try {
-		std::vector<int>::const_iterator vecIt = easyfind(vec1, 17);
+		std::vector<int>::const_iterator vecIt = easyFind(vec1, 17);
 		std::cout << "Found occurrence: " << *vecIt << " in vector\n";
 
-		std::vector<int>::const_iterator nonExistVecIt = easyfind(vec1, 10);
+		std::vector<int>::const_iterator nonExistVecIt = easyFind(vec1, 10);
 		std::cout << "Found: " << *nonExistVecIt << "\n";
 	} catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
+
+	//TESTING WITH LIST CONTAINER
 	std::list<int> lst;
     lst.push_back(10);
     lst.push_back(20);
@@ -28,11 +32,11 @@ int main() {
 
     try {
         // Test finding an existing value in list
-        std::list<int>::const_iterator listIt = easyfind(lst, 30);
+        std::list<int>::const_iterator listIt = easyFind(lst, 30);
         std::cout << "Found occurrence: " << *listIt << " in list\n";
 
         // Test finding a non-existent value in list (will throw exception)
-        std::list<int>::const_iterator nonExistListIt = easyfind(lst, 60);
+        std::list<int>::const_iterator nonExistListIt = easyFind(lst, 60);
 		std::cout << "Found: " << *nonExistListIt << "\n";
     }
     catch (std::exception& e) {
