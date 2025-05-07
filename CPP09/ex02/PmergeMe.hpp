f73936d924b19807f5c4ccb1479de0fc7a7115e9
UnhealthyHuman <jasminaylik@gmail.com>
2025-05-07 11:17:21 +0200

    added comment on hpp file as to the differences between deque and vector and why they were the most suitable container for ex02

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -31,3 +31,41 @@ class PmergeMe {
     std::deque<int>     _tmpDeque; //can access any element in the deque with .at() or []
 
 };
+
+
+//Deque vs Container
+
+//deque (double ended queue) -> container thatv allows for fast insertion
+// and deletion of elements at both ends of the sequence. Provides random
+// access to elements.
+
+//characterists:
+// 1) efficient insertion and deletion at head and tail of container
+// 2) random access with [] operator or .at() method
+// 3) dynamic memory allocation (dynamic resizing) is supported by
+//allocating additional blocks.
+// 4) insertion and deletion at head/tail are fast
+// 5) not contiguous block of memory.
+
+
+//vector -> container that provides random access to its elements and supports
+//dynamic resizing.
+
+//characteristics:
+// 1) stores elements in a single contiguous (sharing a common
+//border; touching (adjacent)) block of memory. Very fast random access.
+// 2) has fast access to the last element but slower for front and middle
+//insertion and deletion due to need to shift elements.
+// 3) dynamic resizing is supported by reallocating memory and copying
+//all existing elements to a larger block.
+
+//Conclusion
+//Both vector and deque allow for dynamic resizing, however the different
+//approach to how they allow for resizing may influence the performance
+//of each container. Vector is preferred when the intent is to be able
+//to quickly randomly access elements and appending elements at the back,
+///if fast insertion and deletion is needed at the front and back of the
+//container, deque is the more suitable option.
+
+//In this case vector and deque were the better suited options for this
+//exercise due to their random access functionality and dynamic resizing. 
